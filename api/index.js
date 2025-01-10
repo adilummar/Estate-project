7a2e7f806714f5ccf04b16bc3955bafef997a745
adilsygmetiv <adilummersygmetiv@gmail.com>
2025-01-10 10:12:31 +0530

    added the test apo

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from './routes/user.route.js'
 
 dotenv.config();
 
@@ -14,6 +15,10 @@ mongoose
     console.log(err);
   });
 
+  app.use('/api/user',userRouter)
+
 app.listen(3000, () => {
   console.log("app is listening to post 3000");
 });
+
+
