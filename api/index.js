161bcd05fe016d2921740c4e950f4b6e9ff0df3e
adilsygmetiv <adilummersygmetiv@gmail.com>
2025-01-10 18:51:31 +0530

    creted signup api route

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from './routes/user.route.js'
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -15,10 +17,9 @@ mongoose
     console.log(err);
   });
 
-  app.use('/api/user',userRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => {
   console.log("app is listening to post 3000");
 });
-
-
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -18,5 +18,5 @@ const userSchema = new mongoose.Schema(
     },
   },{ timestamps: true });
 
-  const User =  mongoose.model('User',userSchma)
+  const User =  mongoose.model('User',userSchema)
 export default User
