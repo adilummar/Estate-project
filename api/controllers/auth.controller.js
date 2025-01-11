fde3b9d7cd8ffee30477d0099ff06203a4feff64
adilsygmetiv <adilummersygmetiv@gmail.com>
2025-01-11 22:44:41 +0530

    complete sign in functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signUp = async (req, res, next) => {
   const { username, password, email } = req.body;
@@ -9,7 +10,25 @@ export const signUp = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(200).json("user created successfuly");
-    console.log('this is submitted')
+    console.log("this is submitted");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "user not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "wrong credentioals"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_token);
+    const {password:pass, ...rest} = validUser._doc
+    res
+      .cookie("access-token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error)
   }
