6247c2b3aa92418a3539c81598e952d966a57784
adilummar <madilummer@gmail.com>
2025-02-20 18:22:17 +0530

    path updated

--- api/index.js
+++ api/index.js
@@ -33,11 +33,11 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/upload", imageupload);
 app.use("/api/listing", listingRouter)
-app.use(express.static(path.join(__dirname,'/client/dist')));
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 
-app.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname,'client','dist','index.html'))
-})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.listen(3000, () => {
   console.log("app is listening to post 3000");
