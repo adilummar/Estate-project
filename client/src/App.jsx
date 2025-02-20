8ecc8820d9b0c3cd3d038d14676e27cf6ae527f8
adilummar <madilummer@gmail.com>
2025-02-20 12:20:42 +0530

    Home issue chaged

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
